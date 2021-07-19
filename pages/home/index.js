7b8f2ba03a5ba44af09e34658bf4fa250d944ec2
Thiago Henrique Fonseca Pereira <thiagop070@gmail.com>
2021-07-19 14:12:47 -0300

    Finalização expand/collapse

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,7 +3,7 @@ var coll = document.getElementsByClassName("collapse")
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active")
-    var content = this.nextElementSibling
+    var content = this.nextElementSibling || this.previousElementSibling
     if (content.style.maxHeight) {
       content.style.maxHeight = null
     } else {
